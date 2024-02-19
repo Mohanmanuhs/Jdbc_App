d61137f3c78b8a5ec5c53b2a28b912ab93004892
Mohanmanuhs <mohanmanuhs9@gmail.com>
2024-02-19 15:13:34 +0530

    connected mysql to java project

--- src/main/java/org/example/JdbcApp.java
+++ src/main/java/org/example/JdbcApp.java
@@ -1,4 +1,25 @@
 package org.example;
 
+import java.sql.*;
+
 public class JdbcApp {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://127.0.0.1:3306/students";
+        String username = "root";
+        String password = "root";
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM student");
+            while (resultSet.next()){
+                System.out.println("Student id : " + resultSet.getInt("st_id"));
+                System.out.println("Student name : " + resultSet.getString("st_name"));
+                System.out.println("Student Course : " + resultSet.getString("st_course"));
+                System.out.println("-------------------------");
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
